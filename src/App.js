ee93030b28e04378de1de0a1a623d79c7d6ec999
Restyled.io <commits@restyled.io>
2024-01-09 07:36:15 +0000

    Restyled by prettier

--- src/App.js
+++ src/App.js
@@ -12,12 +12,16 @@ function App() {
           change this file to see if it will update on the server.
         </p>
         <a
-  className = "App-link"
-  href = "https://reactjs.org"
-  target = "_blank"
-  rel = "noopener noreferrer" > Learn React</a>
-      </header><
-        /div>
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {" "}
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
